4137f16e210be882f72bea88888b0f1b39b464e1
kwak <kmw4690@gmail.com>
2024-03-04 17:12:58 +0900

    nextjs-dashboard4

---
+++ nextjs/nextjs-dashboard/auth.config.ts
@@ -0,0 +1,22 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  // 로그인 하지 않으면 대시보드에 접근 불가.
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
